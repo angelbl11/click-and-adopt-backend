1bef7e3cb6ccc805034921b3a923a596bc00b08c
Angel B <angelbl1611@gmail.com>
2022-06-23 11:09:54 -0500

    modules solved

--- Api/Mutations/UsersMutation.js
+++ Api/Mutations/UsersMutation.js
@@ -2,154 +2,156 @@ const { User } = require("../../DataBase/User");
 const { AdoptedQuestionnarie } = require("../../DataBase/AdoptedQuestionnaire");
 const { AdopterQuestionnarie } = require("../../DataBase/AdopterQuestionnaire");
 const { CONFIG } = require("../../visionClient");
+const path = require("path");
+const fs = require("fs");
 const vision = require("@google-cloud/vision");
 const client = new vision.ImageAnnotatorClient(CONFIG);
 
 module.exports = {
-	addProfilePicture: async (parent, { id, profilePicture }) => {
-		try {
-			const { createReadStream, filename, mimetype, encoding } =
-				await profilePicture;
+  addProfilePicture: async (parent, { id, profilePicture }) => {
+    try {
+      const { createReadStream, filename, mimetype, encoding } =
+        await profilePicture;
 
-			const { ext } = path.parse(filename);
+      const { ext } = path.parse(filename);
 
-			let randomfileName = "";
+      let randomfileName = "";
 
-			for (let i = 0; i < 15; i++) {
-				randomfileName += Math.floor(Math.random() * 1000) + "";
-			}
+      for (let i = 0; i < 15; i++) {
+        randomfileName += Math.floor(Math.random() * 1000) + "";
+      }
 
-			const date = new Date();
-			randomfileName +=
-				date.getDay() + date.getMonth() + date.getFullYear() + "";
+      const date = new Date();
+      randomfileName +=
+        date.getDay() + date.getMonth() + date.getFullYear() + "";
 
-			const stream = createReadStream();
+      const stream = createReadStream();
 
-			const pathName = path.join(
-				__dirname,
-				`../../Images/ProfilePictures/${randomfileName}` + ".jpg"
-			);
+      const pathName = path.join(
+        __dirname,
+        `../../Images/ProfilePictures/${randomfileName}` + ".jpg"
+      );
 
-			await stream.pipe(fs.createWriteStream(pathName));
+      await stream.pipe(fs.createWriteStream(pathName));
 
-			await User.findByIdAndUpdate(id, {
-				profilePicture: {
-					filename: randomfileName + ".jpg",
-					mimetype: mimetype,
-					encoding: encoding,
-				},
-			});
+      await User.findByIdAndUpdate(id, {
+        profilePicture: {
+          filename: randomfileName + ".jpg",
+          mimetype: mimetype,
+          encoding: encoding,
+        },
+      });
 
-			return "Listo";
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	scanPicture: async (parent, { url }) => {
-		client
-			.labelDetection(url)
-			.then((results) => {
-				const labels = results[0].labelAnnotations;
-				console.log("Labels:");
-				labels.forEach((label) => console.log(label.description));
-			})
-			.catch((err) => {
-				console.log("Error:", err);
-			});
-		return "Imagen analizada";
-	},
+      return "Listo";
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  scanPicture: async (parent, { url }) => {
+    client
+      .labelDetection(url)
+      .then((results) => {
+        const labels = results[0].labelAnnotations;
+        console.log("Labels:");
+        labels.forEach((label) => console.log(label.description));
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+    return "Imagen analizada";
+  },
 
-	deletePetInfo: async (parent, { petId }) => {
-		try {
-			await AdoptedQuestionnarie.findByIdAndDelete(petId);
+  deletePetInfo: async (parent, { petId }) => {
+    try {
+      await AdoptedQuestionnarie.findByIdAndDelete(petId);
 
-			return "eliminado";
-		} catch (error) {
-			throw new Error(error);
-		}
-	},
-	addProfilePetPicture: async (parent, { id, petProfilePicture }) => {
-		try {
-			const { createReadStream, filename, mimetype, encoding } =
-				await petProfilePicture;
+      return "eliminado";
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  addProfilePetPicture: async (parent, { id, petProfilePicture }) => {
+    try {
+      const { createReadStream, filename, mimetype, encoding } =
+        await petProfilePicture;
 
-			const { ext } = path.parse(filename);
+      const { ext } = path.parse(filename);
 
-			let randomfileName = "";
+      let randomfileName = "";
 
-			for (let i = 0; i < 15; i++) {
-				randomfileName += Math.floor(Math.random() * 1000) + "";
-			}
+      for (let i = 0; i < 15; i++) {
+        randomfileName += Math.floor(Math.random() * 1000) + "";
+      }
 
-			const date = new Date();
-			randomfileName +=
-				date.getDay() + date.getMonth() + date.getFullYear() + "";
+      const date = new Date();
+      randomfileName +=
+        date.getDay() + date.getMonth() + date.getFullYear() + "";
 
-			const stream = createReadStream();
+      const stream = createReadStream();
 
-			const pathName = path.join(
-				__dirname,
-				`../../Images/ProfilePictures/${randomfileName}` + ".jpg"
-			);
+      const pathName = path.join(
+        __dirname,
+        `../../Images/ProfilePictures/${randomfileName}` + ".jpg"
+      );
 
-			await stream.pipe(fs.createWriteStream(pathName));
+      await stream.pipe(fs.createWriteStream(pathName));
 
-			await AdoptedQuestionnarie.findByIdAndUpdate(id, {
-				petPicture: {
-					filename: randomfileName + ".jpg",
-					mimetype: mimetype,
-					encoding: encoding,
-				},
-			});
+      await AdoptedQuestionnarie.findByIdAndUpdate(id, {
+        petPicture: {
+          filename: randomfileName + ".jpg",
+          mimetype: mimetype,
+          encoding: encoding,
+        },
+      });
 
-			return "Listo";
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	deletePetInfo: async (parent, { petId }) => {
-		try {
-			await AdoptedQuestionnarie.findByIdAndDelete(petId);
+      return "Listo";
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  deletePetInfo: async (parent, { petId }) => {
+    try {
+      await AdoptedQuestionnarie.findByIdAndDelete(petId);
 
-			return "eliminado";
-		} catch (error) {
-			throw new Error(error);
-		}
-	},
-	editUserInfo: async (parent, { editInput, id }) => {
-		try {
-			const { email, fullName, age } = editInput;
-			await User.findByIdAndUpdate(id, {
-				email: email ? email : undefined,
-				fullName: fullName ? fullName : undefined,
-				age: age ? age : undefined,
-			});
-			return "Info actualizada";
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	updateAdopterStatus: async (parent, { id, userStatus }) => {
-		try {
-			console.log(userStatus);
-			await AdopterQuestionnarie.findByIdAndUpdate(id, {
-				isAvailableToAdopt: userStatus,
-			});
-			return "Estado actualizado";
-		} catch (error) {
-			console.log(error);
-		}
-	},
+      return "eliminado";
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  editUserInfo: async (parent, { editInput, id }) => {
+    try {
+      const { email, fullName, age } = editInput;
+      await User.findByIdAndUpdate(id, {
+        email: email ? email : undefined,
+        fullName: fullName ? fullName : undefined,
+        age: age ? age : undefined,
+      });
+      return "Info actualizada";
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  updateAdopterStatus: async (parent, { id, userStatus }) => {
+    try {
+      console.log(userStatus);
+      await AdopterQuestionnarie.findByIdAndUpdate(id, {
+        isAvailableToAdopt: userStatus,
+      });
+      return "Estado actualizado";
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
-	updateAdoptedStatus: async (parent, { id, petStatus }) => {
-		try {
-			console.log(petStatus);
-			await AdoptedQuestionnarie.findByIdAndUpdate(id, {
-				isAvailableToBeAdopted: petStatus,
-			});
-			return "Estado actualizado";
-		} catch (error) {
-			console.log(error);
-		}
-	},
+  updateAdoptedStatus: async (parent, { id, petStatus }) => {
+    try {
+      console.log(petStatus);
+      await AdoptedQuestionnarie.findByIdAndUpdate(id, {
+        isAvailableToBeAdopted: petStatus,
+      });
+      return "Estado actualizado";
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
