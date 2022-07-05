73a00606275ef7b09e013e54758107b0cfed5881
Angel B <angelbl1611@gmail.com>
2022-07-05 13:29:07 -0500

    formated date

--- Api/Mutations/LikeMutation.js
+++ Api/Mutations/LikeMutation.js
@@ -1,21 +1,6 @@
 const { Like } = require("../../DataBase/Like");
 const { LikeUser } = require("../../DataBase/LikeUser");
-
-const date = new Date();
-
-date.setHours(0, 0, 0, 0);
-
-function padTo2Digits(num) {
-  return num.toString().padStart(2, "0");
-}
-
-function formatDate(date) {
-  return [
-    padTo2Digits(date.getDate()),
-    padTo2Digits(date.getMonth() + 1),
-    date.getFullYear(),
-  ].join("-");
-}
+const { formatDate } = require("../../dateFormating");
 
 module.exports = {
   likePet: async (parent, { petId, userId }) => {
---
+++ dateFormating.js
@@ -0,0 +1,15 @@
+const date = new Date();
+
+date.setHours(0, 0, 0, 0);
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
+
+function formatDate(date) {
+  return [
+    padTo2Digits(date.getDate()),
+    date.getFullYear(),
+    padTo2Digits(date.getMonth() + 1),
+  ].join("-");
+}
