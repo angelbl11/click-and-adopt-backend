b01264f610769747e7860e6357ca22df5168d819
Angel B <angelbl1611@gmail.com>
2022-07-24 19:56:04 -0500

    testing cloud vision api v10

--- Api/Mutations/UsersMutation.js
+++ Api/Mutations/UsersMutation.js
@@ -83,7 +83,7 @@ module.exports = {
       )) == false
     ) {
       throw new Error(
-        "Por favor, utilice una fotografía suya; además, se recomienda que en esta se aprecie correctamente el rostro, sin accesorios que lo obstruyan y se pueda visualizar de frente."
+        "Por favor, utilice una fotografía suya; además, se recomienda que en esta se aprecie correctamente el rostro, sin accesorios que lo obstruyan y se pueda visualizar de frente. En caso de que la fotografía que intenta subir cumpla los requisitos, por favor, vuelva a intentar el proceso."
       );
     }
 
