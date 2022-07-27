eb1022084012674ad7052b882fb763e0f997611b
Angel B <angelbl1611@gmail.com>
2022-07-27 02:11:23 -0500

    .

--- Api/Mutations/LikeMutation.js
+++ Api/Mutations/LikeMutation.js
@@ -18,10 +18,10 @@ module.exports = {
     const likes = await Like.find({ userId: userId });
 
     likes.map((item) => {
-      if (petId == item.petId) throw new Error("Like repetido");
+      if (petId == item.petId) throw new Error("Like repetido.");
     });
 
-    if (likes.length > 10) throw new Error("Limite de likes excedido");
+    if (likes.length > 10) throw new Error("Limite de likes excedido.");
 
     const petOwner = await AdoptedQuestionnarie.findById(petId);
     const user = await AdopterQuestionnarie.findOne({ userId: userId });
