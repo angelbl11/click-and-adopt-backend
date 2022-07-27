945b2f9bbd6dc6e9bd516e16a8d15d2a145bd0b2
Angel B <angelbl1611@gmail.com>
2022-07-27 00:05:22 -0500

    like & delete chat

--- Api/Mutations/LikeMutation.js
+++ Api/Mutations/LikeMutation.js
@@ -6,90 +6,90 @@ const { Match } = require("../../DataBase/Match");
 const { AdopterQuestionnarie } = require("../../DataBase/AdopterQuestionnaire");
 
 const match = async (adopterInfo, petOwnerInfo, petInvolved) => {
-	await new Match({
-		adopterInfo: adopterInfo,
-		petOwnerInfo: petOwnerInfo,
-		petInvolved: petInvolved,
-	}).save();
+  await new Match({
+    adopterInfo: adopterInfo,
+    petOwnerInfo: petOwnerInfo,
+    petInvolved: petInvolved,
+  }).save();
 };
 
 module.exports = {
-	likePet: async (parent, { petId, userId }) => {
-		const likes = await Like.find({ userId: userId });
+  likePet: async (parent, { petId, userId }) => {
+    const likes = await Like.find({ userId: userId });
 
-		likes.map((item) => {
-			if (petId == item.petId) throw new Error("Like repetido");
-		});
+    likes.map((item) => {
+      if (petId == item.petId) throw new Error("Like repetido");
+    });
 
-		if (likes.length > 10) throw new Error("Limite excedido");
+    if (likes.length > 10) throw new Error("Limite excedido");
 
-		const petOwner = await AdoptedQuestionnarie.findById(petId);
+    const petOwner = await AdoptedQuestionnarie.findById(petId);
 
-		const ownerLikes = await LikeUser.findOne({
-			userId: petOwner.userId,
-			likedUserId: userId,
-		});
+    const ownerLikes = await LikeUser.findOne({
+      userId: petOwner.userId,
+      likedUserId: userId,
+    });
 
-		if (ownerLikes) {
-			await match(userId, petOwner.userId, petId);
+    if (ownerLikes) {
+      await match(userId, petOwner.userId, petId);
 
-			await LikeUser.findOneAndDelete({
-				userId: petOwner.userId,
-				likedUserId: userId,
-			});
+      await LikeUser.findOneAndDelete({
+        userId: petOwner.userId,
+        likedUserId: userId,
+      });
 
-			return "Match";
-		}
+      return "Match";
+    }
 
-		await new Like({
-			petId: petId,
-			userId: userId,
-			date: formatDate(new Date()),
-		}).save();
+    await new Like({
+      petId: petId,
+      userId: userId,
+      date: formatDate(new Date()),
+    }).save();
 
-		return "Like";
-	},
+    return "Like";
+  },
 
-	likeUser: async (parent, { userId, likedUserId }) => {
-		const likes = await LikeUser.find({ userId: userId });
+  likeUser: async (parent, { userId, likedUserId }) => {
+    const likes = await LikeUser.find({ userId: userId });
 
-		likes.map((item) => {
-			if (likedUserId == item.likedUserId) throw new Error("Like repetido");
-		});
+    likes.map((item) => {
+      if (likedUserId == item.likedUserId) throw new Error("Like repetido");
+    });
 
-		if (likes.length > 10) throw new Error("Limite excedido");
+    if (likes.length > 10) throw new Error("Limite excedido");
 
-		const adopterId = await AdopterQuestionnarie.findById(likedUserId);
+    const adopterId = await AdopterQuestionnarie.findById(likedUserId);
 
-		const adopterLikes = await Like.find({ userId: adopterId.userId });
+    const adopterLikes = await Like.find({ userId: adopterId.userId });
 
-		for (const item of adopterLikes) {
-			const checkIfMatch = await AdoptedQuestionnarie.findById(item.petId);
+    for (const item of adopterLikes) {
+      const checkIfMatch = await AdoptedQuestionnarie.findById(item.petId);
 
-			if (checkIfMatch.userId == userId) {
-				await match(adopterId.userId, userId, item.petId);
+      if (checkIfMatch.userId == userId) {
+        await match(adopterId.userId, userId, item.petId);
 
-				await Like.findOneAndDelete({
-					petId: item.petId,
-					userId: adopterId.userId,
-				});
+        await Like.findOneAndDelete({
+          petId: item.petId,
+          userId: adopterId.userId,
+        });
 
-				return "match";
-			}
-		}
+        return "Match";
+      }
+    }
 
-		await new LikeUser({
-			userId: userId,
-			likedUserId: likedUserId,
-			date: formatDate(new Date()),
-		}).save();
+    await new LikeUser({
+      userId: userId,
+      likedUserId: likedUserId,
+      date: formatDate(new Date()),
+    }).save();
 
-		return "like";
-	},
+    return "Like";
+  },
 
-	deleteMatch: async (p, { matchId }) => {
-		await Match.findByIdAndDelete(matchId);
+  deleteMatch: async (p, { matchId }) => {
+    await Match.findByIdAndDelete(matchId);
 
-		return "deleted";
-	},
+    return "Deleted";
+  },
 };
