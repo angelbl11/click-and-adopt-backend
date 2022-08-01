d73573b6ea71b0b84625d1630724a5a336ace441
Angel B <angelbl1611@gmail.com>
2022-08-01 02:29:50 -0500

    please work

--- Auth/CheckIfIsAuth.js
+++ Auth/CheckIfIsAuth.js
@@ -2,18 +2,18 @@ const { verify } = require("jsonwebtoken");
 const { KEY } = require("../config");
 
 const checkIfIsAuth = (context) => {
-	const header = context.req.headers.authorization;
+  const header = context.req.headers.authorization;
 
-	if (!header) throw new Error("Autorización no proporcionada");
+  if (!header) throw new Error("Autorización no proporcionada");
 
-	const token = header.split("Bearer")[1];
+  const token = header.split("Bearer")[1];
 
-	if (!token) throw new Error("Token no proporcionado");
+  if (!token) throw new Error("Token no proporcionado");
 
-	try {
-		const user = verify(token, KEY);
-		return user;
-	} catch (error) {
-		throw new Error("Token expirado");
-	}
+  try {
+    const user = verify(token, KEY);
+    return user;
+  } catch (error) {
+    throw new Error("Token expirado");
+  }
 };
